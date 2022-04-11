6a76ab4b6dc2df61250caecdbf5c4da9d924bef3
ricky <ricky_chou@mail.chungyo.net>
2022-04-11 16:31:16 +0800

    [service 範例]

---
+++ app/Services/DashBoard/DashBoardService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services\DashBoard;
+
+//  留言板相關邏輯
+class DashBoardService
+{
+    
+}
---
+++ app/Services/User/UserService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\User;
+
+//  會員相關邏輯
+class UserService
+{
+    public function getTest()
+    {echo 123;
+    if($test==123){
+        echo 456;
+    }
+    }
+}
