6b7c44f26fb7089eaa0386abf6bbffdf5be3c7f9
Jarek <jarek_zhou@mail.chungyo.net>
2022-04-13 16:47:20 +0800

    0413_jarek

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+// use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,16 +16,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+    //  將預設網址改為 resource >> view >> 下面的home
+    ///   初始預設為welcome
 });
 
 Route::get('/test', function () {
-    return 'Hello Jarek!!';
+    return 'Hello jarek!!';
 });
+
+
 
 Route::get('/message', 'TestController@getTest');
 Route::get('/gamecode', 'Rookie\MessageController@getGameCodeList');
 Route::get('/gamecode/{gametype}', 'Rookie\MessageController@getGameCodeByGameType');
+
+
 Auth::routes();
+// 會員註冊登入的路徑
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('/posts',PostController::class)->middleware('auth'); 
