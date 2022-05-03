6cf952e9c44f148e2ab9b32f2bb3ffe8fe18f697
po_lee <po_lee@mail.chungyo.net>
2022-05-03 15:36:08 +0800

    refactor

--- app/Services/Rookie/BowlingService.php
+++ app/Services/Rookie/BowlingService.php
@@ -7,16 +7,15 @@ class BowlingService
     public function getBowlingList(array $_aInput)
     {
         $aResult = [];
-        foreach ($_aInput as $aInput) {
+        foreach ($_aInput as $key => $aInput) {
             $aScore[] = $aInput[0] + $aInput[1];
-        }
 
-        $aResult[0] = $aScore[0];
-        if (count($aScore) > 1) {
-            $aResult[1] = $aScore[0] + $aScore[1];
-        }
-        if (count($aScore) > 2){
-            $aResult[2] = $aScore[0] + $aScore[1] + $aScore[2];
+            if(count($aScore) == 1){
+                $aResult[0] = $aScore[0];
+            }
+            if(count($aScore) > 1){
+                $aResult[$key] = $aResult[$key-1] + $aScore[$key];
+            }
         }
         return $aResult;
 
