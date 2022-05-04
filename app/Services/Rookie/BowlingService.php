33c50e8e6190869c322089960cc71f4e05bd58a5
po_lee <po_lee@mail.chungyo.net>
2022-05-04 09:45:09 +0800

    refactor

--- app/Services/Rookie/BowlingService.php
+++ app/Services/Rookie/BowlingService.php
@@ -9,20 +9,20 @@ class BowlingService
         $aResult = [];
         foreach ($_aInput as $key => $aInput) {
             $aScore[$key] = $aInput[0] + $aInput[1];
-
-            if (count($aScore) == 1) {          //判斷是否為第一局
-                if ($aScore[$key] == 10) {      //判斷是否為spare
+            
+            if ($aScore[$key] == 10) {          //判斷是否為spare
+                if (count($aScore) == 1) {      //判斷是否為第一局
                     $aResult[$key] = $aScore[$key] + $_aInput[$key+1][0];
-                }else{                          
-                    $aResult[$key] = $aScore[$key];
+                }else{
+                    $aResult[$key] = $aResult[$key-1] + $aScore[$key] + $_aInput[$key+1][0];
                 }
             }
-            if (count($aScore) > 1) {
-                if ($aScore[$key] == 10) {      //判斷是否為spare
-                    $aResult[$key] = $aResult[$key-1] + $aScore[$key] + $_aInput[$key+1][0];
+            elseif ($aScore[$key] != 10) {      //一般情況
+                if (count($aScore) == 1) {      //判斷是否為第一局
+                    $aResult[$key] = $aScore[$key];
                 }else{
-                $aResult[$key] = $aResult[$key-1] + $aScore[$key]; 
-                }        
+                    $aResult[$key] = $aResult[$key-1] + $aScore[$key]; 
+                }
             }
         }
         return $aResult;
